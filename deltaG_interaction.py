6872d551988f6ecab79342a5462f17228c960e49
magdalenq <78900119+magdalenq@users.noreply.github.com>
2022-02-03 14:36:08 +0100

    Update deltaG_interaction.py

--- deltaG_interaction.py
+++ deltaG_interaction.py
@@ -2,25 +2,30 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+def deltaG(seq):
+    link = "https://dgpred.cbr.su.se/analyze.php?with_length=on&seq=" + seq
+
+    requete = requests.get(link)
+    page = requete.content
+    soup = BeautifulSoup(page, 'html.parser')
+
+    data = soup.table
+
+    #res = re.compile(r'^([\w]*)(<b><span style="color: green">)([+-]\w.\w{3,})(</span></b></big></td></tr>)$')
+    res = re.compile(r'\w*\t=\t\w*')
+    #print(res)
+    for all in data:
+        if re.match(res, str(all)) != None:
+            print(all)
+            #pass
+    print(data)
+
 seq = "ATEYIGYAWAMVVVIIGATIGIKLFKK"
-link = "https://dgpred.cbr.su.se/analyze.php?with_length=on&seq=" + seq
+print(deltaG(seq))
+    #<span style="color: green">-0.61</span>
+    #/html/body/table/tbody/tr[3]/td/table/tbody/tr[51]/td/big/b/span
 
-requete = requests.get(link)
-page = requete.content
-soup = BeautifulSoup(page, 'html.parser')
 
-data = soup.table
 
-#res = re.compile(r'^([\w]*)(<b><span style="color: green">)([+-]\w.\w{3,})(</span></b></big></td></tr>)$')
-#res = re.compile(r'([+-])\w.\w{3,}')
-#print(res)
-for all in data:
-    if re.match(res, str(all)) != None:
-        print(all)
-        #pass
-#print(data)
-
-#<span style="color: green">-0.61</span>
-#/html/body/table/tbody/tr[3]/td/table/tbody/tr[51]/td/big/b/span
 
 
