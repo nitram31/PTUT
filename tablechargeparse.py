83f57ba0f05ecb4904034b9fa8a9f2de1c2ff1f9
magdalenq <78900119+magdalenq@users.noreply.github.com>
2022-03-10 17:29:10 +0100

    Update and rename table.py to tablechargeparse.py

--- tablechargeparse.py
+++ tablechargeparse.py
@@ -3,12 +3,7 @@ import pandas as pd
 df = pd.read_excel("results_full.xlsx")
 df_csv = df.to_csv(index=False)
 
-
-# Filter on  column 2 - targetpred selection which is not MT
-def nopreseq(dataframe = df):
-    nopreseq = df[df['targetp_pred'].str.contains('SP') | df['targetp_pred'].str.contains('OTHER')]
-    return(nopreseq)
-
+# returns rows with charge discard
 def chargeparse(df, classe):
     if classe == "classe1":
         classe = df[df['charge'].astype(int) > 0 ]
