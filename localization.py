1980b25ed2a86e1bcd94f0db878505411bd9b9c8
magdalenq <78900119+magdalenq@users.noreply.github.com>
2022-03-31 17:01:30 +0200

    le lion est mort ce soir

--- localization.py
+++ localization.py
@@ -6,20 +6,31 @@ panda doc : https://pandas.pydata.org/docs/user_guide/merging.html#merging-join
 def creertable(nosprot, locprot):
     for lineLoc in locprot:
         for lineNosProt in nosprot:
-            if lineLoc["Uniprot ID"] != lineNosProt["Uniprot ID"]:
-                lineLoc["Uniprot ID"].pop
+            if lineLoc[2] != lineNosProt[1]:
+                locprot = locprot.drop(locprot[lineLoc])
     return(locprot)
-"""
+
 def creertable(nosprot, locprot):
-    res = pd.merge(locprot, nosprot, how="inner",on = "Uniprot ID", right_on=True)
+    res = pd.merge(locprot, nosprot, on ="UniProt_ID")
+    return(res)
+def creertable(nosprot, locprot):
+    dataframe = [nosprot, locprot]
+    res = pd.concat(dataframe, join="inner", keys="UniProt_ID")
+    return (res)    
+"""
+
+def creertable(nosprot, locprot):
+    res = pd.merge(locprot, nosprot, how="outer", on="UniProt_ID")
     return(res)
 
 
-
 if __name__ == "__main__":
-    nosprot = pd.read_excel("sequences3.xlsx")
-    nosprot.to_csv("sequences3.csv", sep=';', index=False)
-    locprot = pd.read_excel("localisation_S2.xlsx")
-    locprot.to_csv("localisation_S2.csv", sep=';', index=False)
+    nosprot = pd.read_csv("sequences3.csv")
+    locprot = pd.read_csv("localisation_S2.csv")
 
+    nosprot.columns = nosprot.columns.str.strip()
+    locprot.columns = locprot.columns.str.strip()
+
+    print("nosprot : ", nosprot.columns.tolist())
+    print("locprot : ", locprot.columns.tolist())
     creertable(nosprot, locprot).to_csv("tab_localizations.csv", sep="\t", index=False) #export file
