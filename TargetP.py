3c817a24b57a8ca0954d66aa8fd65284e8231057
Magdalena Szczuka <magda.szczuka@gmail.com>
2022-04-28 16:12:39 +0200

    comments TargetP.py

--- TargetP.py
+++ TargetP.py
@@ -1,29 +1,23 @@
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-import requests
 import re
-
-
-def get_targetp_results(fasta_file):
-    url = "https://services.healthtech.dtu.dk/service.php?TargetP-2.0"
-    test_response = requests.post(url, files={"form_field_name": test_file})
-
 
 def parse_targetp(targetp_result, seq_dict):
     pat = re.compile(r'(^\w{6})\t(\w{2,5})\t(.{8})\t(.{8})\t(.{8})')
-    with open(targetp_result) as file:
+    with open(targetp_result) as file: #targetp_result is a file containing informations about every protein from official TargetP site
+        #this informations were obtained with web scraping in Test_GUI script
         for line in file:
-            matched_line = re.match(pat, line)
+            matched_line = re.match(pat, line) #getting result with regex
             if matched_line != None:
 
-                prot_id = matched_line.group(1)
-                targetp_pred = matched_line.group(2)
+                prot_id = matched_line.group(1) #protein ID
+                targetp_pred = matched_line.group(2)  #class of protein (OTHER, SP, TM)
                 if targetp_pred == "MT":
                     targetp_pred_score = matched_line.group(5)
                 elif targetp_pred == "SP":
                     targetp_pred_score = matched_line.group(4)
-                else:
+                else:   #case for transmembrane proteins so our proteins of interest
                     targetp_pred_score = matched_line.group(3)
                     for current_id in seq_dict.keys():
                         if prot_id in current_id:
