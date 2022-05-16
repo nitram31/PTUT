6ffbab6d0f17d8d8ac60e93a7a5c28321df684ee
RACOUPEAU Martin <nitram31300@gmail.com>
2022-05-16 13:52:57 +0200

    modified class.py

--- Class.py
+++ Class.py
@@ -1,10 +1,23 @@
 import pandas as pd
+from Mylib.PTUT import Fastaboy
+import TargetP
 """ not used but saved as archive for future modifications"""
+
 
 def class_predictor(seq_dict):
     id_list_class_1 = []
     id_list_class_2 = []
+    id_list_class_3 = []
+
+    predictor_list_1 = []
+    predictor_list_2 = []
+
     for current_id in seq_dict.keys():
+        predictor_name_1 = []
+        predictor_name_2 = []
+
+        seq_dict[current_id]['score'] = 0
+        seq_dict[current_id]['class'] = 'Other'
         keys = seq_dict[current_id]
         tm_results = []
         for key in keys:
@@ -19,80 +32,87 @@ def class_predictor(seq_dict):
             while software_TM_pred[j] != '_':
                 program_name += software_TM_pred[j]
                 j += 1
+            if seq_dict[current_id]['targetp_pred'] != 'MT':
+                if tm_pred.count('M') == 1:
 
-            if tm_pred.count('M') == 1:
-                try:
-                    if seq_dict[current_id][program_name + '_plus_10_after_TM_charge'][0] > 0:
-                        if seq_dict[current_id]['targetp_pred'] != 'MT':
-                            id_list_class_1.append(current_id)
-                except IndexError:
-                    pass
+                        id_list_class_1.append(current_id)
+                        predictor_name_1.append(program_name)
+                elif tm_pred.count('M') == 2:
 
-            if tm_pred.count('M') == 2:
-                try:
-                    if seq_dict[current_id][program_name + '_plus_10_after_TM_charge'][0] > 0:
-                        if seq_dict[current_id]['targetp_pred'] != 'MT':
-                            id_list_class_2.append(current_id)
-                except IndexError:
-                    pass
+                    id_list_class_2.append(current_id)
+                    predictor_name_2.append(program_name)
+            else:
+                if tm_pred.count('M') > 1:
+                    id_list_class_3.append(current_id)
+        predictor_list_1 += predictor_name_1
+        predictor_list_2 += predictor_name_2
 
-    id_list_class_2 = list(dict.fromkeys(id_list_class_2))
-    id_list_class_1 = list(dict.fromkeys(id_list_class_1))
 
-    table_values = []
-    col_names = []
-
-    first_prot = list(seq_dict.keys())[0]
-    for col in seq_dict[first_prot]:
-        col_names.append(col)
-    col_names[0] = 'name'
-
-    for key in id_list_class_1:
-        line = []
-        key_list = [key]
-        for variable_names in seq_dict[key]:
-            if variable_names != 'seq':
-                key_list.append(seq_dict[key][variable_names])
-        line.append(key_list)
-        table_values += line
-    content = pd.DataFrame(table_values, columns=col_names)
-    content.to_csv('Class1.csv', sep=";")
-
-    for key in id_list_class_2:
-        line = []
-        key_list = [key]
-        for variable_names in seq_dict[key]:
-            if variable_names != 'seq':
-                key_list.append(seq_dict[key][variable_names])
-        line.append(key_list)
-        table_values += line
-    content = pd.DataFrame(table_values, columns=col_names)
-    content.to_csv('Class2.csv', sep=";")
+    for current_id in seq_dict.keys():
+        if current_id in id_list_class_1 and current_id not in id_list_class_2:
+            seq_dict[current_id]['class'] = 'class 1'
+            seq_dict[current_id]['score'] += 0.5
 
 
 
+        elif current_id in id_list_class_2 and current_id not in id_list_class_1:
+            seq_dict[current_id]['class'] = 'class 2'
+            seq_dict[current_id]['score'] += 0.5
+
+        elif current_id in id_list_class_2 and current_id in id_list_class_1:
+            seq_dict[current_id]['class'] = 'class 2'
+            seq_dict[current_id]['score'] += 0.25
+
+        elif current_id in id_list_class_3:
+            seq_dict[current_id]['class'] = 'class 3'
+            seq_dict[current_id]['score'] += 0.5
+
+        """id_list_class_2 = list(dict.fromkeys(id_list_class_2))
+            id_list_class_1 = list(dict.fromkeys(id_list_class_1))"""
+
+        """table_values = []
+        col_names = []
+
+        first_prot = list(seq_dict.keys())[0]
+        for col in seq_dict[first_prot]:
+            col_names.append(col)
+        col_names[0] = 'name'
+
+        for key in id_list_class_1:
+            line = []
+            key_list = [key]
+            for variable_names in seq_dict[key]:
+                if variable_names != 'seq':
+                    key_list.append(seq_dict[key][variable_names])
+            line.append(key_list)
+            table_values += line
+        content = pd.DataFrame(table_values, columns=col_names)
+        content.to_csv('Class1.csv', sep=";")"""
+
+        """if seq_dict[current_id]['tmhmm_TM_pred'].count('M') == 2 \
+        or seq_dict[current_id]['HMMTOP_TM_pred'].count('M') == 2 \
+        or seq_dict[current_id]['DeltaG_TM_pred'].count('M') == 2:
+            try:
+
+                if seq_dict[current_id]['tmhmm_plus_10_after_TM_charge'][0] > 0 \
+                or seq_dict[current_id]['HMMTOP_plus_10_after_TM_charge'][0] > 0 \
+                or  seq_dict[current_id]['DeltaG_plus_10_after_TM_charge'][0] > 0:
+                    if seq_dict[current_id]['targetp_pred'] != 'MT':
+                        cpt += 1
+                        print(cpt)
+            except IndexError:
+                pass"""
+    return seq_dict
 
 
 
+if __name__ == "__main__":
+    seq_dict = Fastaboy.fasta_reader("Yeast_proteome.txt")
 
-
-
-
-
-
-
-
-    """if seq_dict[current_id]['tmhmm_TM_pred'].count('M') == 2 \
-    or seq_dict[current_id]['HMMTOP_TM_pred'].count('M') == 2 \
-    or seq_dict[current_id]['DeltaG_TM_pred'].count('M') == 2:
-        try:
-
-            if seq_dict[current_id]['tmhmm_plus_10_after_TM_charge'][0] > 0 \
-            or seq_dict[current_id]['HMMTOP_plus_10_after_TM_charge'][0] > 0 \
-            or  seq_dict[current_id]['DeltaG_plus_10_after_TM_charge'][0] > 0:
-                if seq_dict[current_id]['targetp_pred'] != 'MT':
-                    cpt += 1
-                    print(cpt)
-        except IndexError:
-            pass"""
-
+    seq_dict = TargetP.parse_targetp(r"C:\Users\Martin\PycharmProjects\pythonProject\Mylib\PTUT\scere_summary.targetp2",
+                                     seq_dict)
+    # seq_dict = TargetP.parse_targetp(r"D:\Bureau\Cours\M1\pythonProject\Mylib\PTUT\scere_summary.targetp2",
+    # seq_dict)
+    seq_dict = Fastaboy.csv_parser(seq_dict, r'C:\Users\Martin\PycharmProjects\pythonProject\Mylib\PTUT\output.csv', sep=";")
+    seq_dict = class_predictor(seq_dict)
+    print(seq_dict)
--- panda.py
+++ panda.py
@@ -1,11 +1,13 @@
-import pandas as pd 
+import pandas as pd
+
 """ not used but saved as archive for future modifications"""
+
 
 def chargeparse(df, classe):
     if classe == 1:
-        classe = df[df['charge'].astype(int) > 0 ] #for class 1
+        classe = df[df['charge'].astype(int) > 0]  # for class 1
     elif classe == 2:
-        classe = df[df['charge'].astype(int) > 1 ] #for class 2
+        classe = df[df['charge'].astype(int) > 1]  # for class 2
     return (classe)
 
 
@@ -15,22 +17,37 @@ def nopresequence(df):
     df_nopreseq = df[(df['targetp_pred'].str.contains('SP')) | (df['targetp_pred'].str.contains('OTHER'))]
     return df_nopreseq
 
+
 # Decompte du nombre de TM
 
-def numberTM(df, prot_class):
+def numberTM(df):
+
     # first class protein with one TM
-    if prot_class == 1:
-        df = df[(df['DeltaG+HMMTOP_TM_pred'].str.count('M') == 1) | (df['Tmhmm_pred'].str.count('M') == 1)]
-    elif prot_class == 2:
-        df = df[(df['DeltaG+HMMTOP_TM_pred'].str.count('M') == 2) | (df['Tmhmm_pred'].str.count('M') == 2)]
+    class_1 = df[(df['DeltaG_TM_pred'].str.count('M') == 1)
+                 | (df['tmhmm_TM_pred'].str.count('M') == 1)
+                 | (df['HMMTOP_TM_pred'].str.count('M') == 1)]
+    print(class_1)
+    for index, row in class_1.iterrows():
+        df.iloc[index]['class'] = 'class 1'
+        print(df.iloc[index])
+        break
+    class_2 = df[(df['DeltaG_TM_pred'].str.count('M') == 2)
+                           | (df['tmhmm_TM_pred'].str.count('M') == 2)
+                           | (df['HMMTOP_TM_pred'].str.count('M') == 2)]
+    for index, row in class_2.iterrows():
+        df.iloc[index]['class'] = 'class 2'
     return df
+
 
 if __name__ == "__main__":
     df = pd.read_excel(r"C:\Users\Martin\PycharmProjects\pythonProject\Mylib\PTUT\output.xlsx")
-    df_nopreseq = nopresequence(df)
+    df['class'] = 'Other'
+    df = numberTM(df)
+
+    """df_nopreseq = nopresequence(df)
     df_TMclass1 = numberTM(df_nopreseq, prot_class=1)
     df_class1 = chargeparse(df_TMclass1, 1)
     df_TMclass2 = numberTM(df_nopreseq, prot_class=2)
     df_class2 = chargeparse(df_TMclass2, 2)
     df_class1.to_csv("classe1.csv", sep=";", index=False)  # export file
-    df_class2.to_csv("classe2.csv", sep=";", index=False)  # export file
+    df_class2.to_csv("classe2.csv", sep=";", index=False)  # export file"""
